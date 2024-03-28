a31e20bf59978a1a54422555e29ac094d181af50
adnan4k <fayomuhe5@gmail.com>
2024-03-28 21:25:45 +0300

    port number added

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 const sequilize = new Sequelize(process.env.DB_REMOTE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  port:process.env.MYSQL_PORT,
   logging:false, // Toggle logging if needed
 });
 
